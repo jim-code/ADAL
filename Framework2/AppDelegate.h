c99d69c02c68061fee48384869c7466dcad680ef
Jim Jin <jjin@s-34363b73827a.local>
2017-03-14 08:55:45 +0800

    Initial Commit

---
+++ Framework2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Framework2
+//
+//  Created by Jim Jin [STAFF] on 3/14/17.
+//  Copyright © 2017 Jim Jin [STAFF]. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Framework2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Framework2
+//
+//  Created by Jim Jin [STAFF] on 3/14/17.
+//  Copyright © 2017 Jim Jin [STAFF]. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
